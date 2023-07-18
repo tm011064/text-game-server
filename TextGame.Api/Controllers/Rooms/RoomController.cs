7bafe7a28c88907d190274ad94c6a54d6a54968c
tm011064 <majewski.roman@hotmail.co.uk>
2023-07-18 15:48:14 +0100

    server

---
+++ TextGame.Api/Controllers/Rooms/RoomController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TextGame.Api.Controllers.Rooms
+{
+    [ApiController]
+    [Route("rooms")]
+    public class RoomController : ControllerBase
+    {
+        private readonly ILogger<RoomController> logger;
+
+        public RoomController(ILogger<RoomController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "WOHOO";
+        }
+    }
+}
