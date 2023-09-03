e0a3536eceda63dd7c8131f7a27031cc12889a95
Roman Majewski <tm011064@Romans-MacBook-Pro.local>
2023-09-03 13:01:57 +0100

    c

--- TextGame.Data/Contracts/Locations/Location.cs
+++ TextGame.Data/Contracts/Locations/Location.cs
@@ -1,11 +1,26 @@
-﻿using TextGame.Data.Contracts.Chapters;
+﻿using System.Text.Json.Serialization;
+using TextGame.Data.Contracts.Chapters;
+using TextGame.Data.Contracts.Games;
 using TextGame.Data.Contracts.Navigation;
+using TextGame.Data.Sources;
 
 namespace TextGame.Data.Contracts.Locations;
 
-public record Location(string Key)
+public interface ILocation
 {
-    public IReadOnlyCollection<NavigationCommand> NavigationCommands { get; init; } = Array.Empty<NavigationCommand>();
+    string Key { get; }
+
+    IGame Game { get; }
+
+    LocalizedContentProvider<IReadOnlyCollection<NavigationCommand>> LocalizedNavigationCommands { get; }
+}
+
+public record LocationBuilder(
+    string Key,
+    IReadOnlyCollection<NavigationCommand>? NavigationCommands)
+{
+    [JsonIgnore]
+    public LocalizedContentProvider<IReadOnlyCollection<NavigationCommand>> LocalizedNavigationCommands { get; init; } = null!;
 
     public IReadOnlyCollection<LocationGameObject> Objects { get; init; } = Array.Empty<LocationGameObject>();
 
